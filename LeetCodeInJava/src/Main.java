779fd67b0e8e22554803231a9992a1cb5bfc276d
SunCheng <6518512@qq.com>
2019-12-22 15:47:17 +0800

    '1222'

--- LeetCodeInJava/src/Main.java
+++ LeetCodeInJava/src/Main.java
@@ -1,12 +1,48 @@
 
+import java.util.*;
+
 /**
  * @Classname Main
  * @Description TODO
  * @Date 2019/12/18 19:22
  * @Created by SunCheng
  */
-public class Main  {
+public class Main {
     public static void main(String[] args) {
+        System.out.println(Integer.toBinaryString(1000000007));
+    }
 
+    public static boolean isPossibleDivide(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
+        for (int val : nums) {
+            if (map.containsKey(val)) {
+                map.put(val, map.get(val) + 1);
+            }else
+                map.put(val, 1);
+            min = val<min?val:min;
+        }
+        int start = min;
+        while (!map.isEmpty()) {
+            for (int i = 0; i < k; i++) {
+                int num = start+i;
+                if (!map.containsKey(num)) {
+                    return false;
+                }
+                if (map.get(num) == 1) {
+                    map.remove(num);
+                } else {
+                    map.put(num, map.get(num) - 1);
+                }
+            }
+            for (int i = 0; i < map.size(); i++) {
+                if (!map.containsKey(start + i)) {
+                    start++;
+                } else {
+                    break;
+                }
+            }
+        }
+        return true;
     }
 }
---
+++ LeetCodeInJava/src/three/Solution387.java
@@ -0,0 +1,22 @@
+package three;
+
+/**
+ * @Classname Solution387
+ * @Description 字符串中的第一个唯一字符
+ * @Date 2019/12/22 10:00
+ * @Author SunCheng
+ */
+public class Solution387 {
+    public int firstUniqChar(String str) {
+        if (str == null) return -1;
+        int[] arrayMap = new int[128];
+        for (int i = 0; i < str.length(); i++) {
+            arrayMap[str.charAt(i)]++;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (arrayMap[str.charAt(i)] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
