62e10c0ca01d788df1e5c201c4b4e4ff0219608d
SunCheng <6518512@qq.com>
2020-01-08 08:27:57 +0800

    '208实现Trie'

--- LeetCodeInJava/src/Main.java
+++ LeetCodeInJava/src/Main.java
@@ -9,8 +9,27 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        char[] chars = sc.nextLine().toCharArray();
+        char[] directionL = {'N', 'W', 'S', 'E'};
+        char[] directionr = {'N', 'E', 'S', 'W'};
 
+        // L,R
+        int[] turn = {0,0};
+        for (char c :
+                chars) {
+            if (c == 'L')
+                turn[0]++;
+            else
+                turn[1]++;
+        }
+        boolean L = true;
+        if(turn[0]<turn[1])L = false;
+        int index = Math.abs(turn[0]-turn[1])%4;
+        if(L)
+            System.out.println(directionL[index]);
+        else
+            System.out.println(directionr[index]);
     }
-
-
 }
